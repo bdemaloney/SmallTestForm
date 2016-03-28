f893f5b7f45697853cfd79374f6b0fcb30a4ba2f
bdemaloney <bdemaloney@gmail.com>
2016-03-28 11:10:00 -0400

    Added files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+angular.module('mainApp',['ngResource']);
+
+/*
+// Was going to try to store a person object. 
+function add(name, email, telephone, street, city, state, zip) {
+
+  var personObj = {
+    'name': $scope.name,
+    'email': $scope.email,
+    'telephone': $scope.telephone,
+    'street': $scope.street,
+    'city': $scope.city,
+    'state': $scope.state,
+    'zip': $scope.zip
+  };
+alert(personObj.name);
+  }
+*/
